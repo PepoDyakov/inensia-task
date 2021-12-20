702217a1b6e0613d760da9e61ac812aebf18f181
PepoDyakov <petar.dyakov@icloud.com>
2021-12-20 14:15:02 +0200

    inital commit

---
+++ C#/Task 1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace ReadJSONTask { 
+    class Program
+    {
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Nationality { get; set; }
+            public string Sex { get; set; }
+            public DateTime DateofBirth { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Person> people = JsonConvert.DeserializeObject<List<Person>>(File.ReadAllText("input.txt"));
+            foreach(var person in people)
+            {
+                var dob = person.DateofBirth.Year;
+                var currentYear = DateTime.Now.Year;
+                var age = currentYear - dob;
+                Console.WriteLine("{0} {1} \n{2} \n{3} \n{4} years old \n", person.Name, person.Surname, person.Sex, person.Nationality, age);
+            }
+        }
+    }
+}
---
+++ C#/Task 2/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter gross salary amount:");
+            var input = Console.ReadLine();
+            double grossAmount = Convert.ToDouble(input);
+
+            double netAmount = CalculateTaxes(grossAmount);
+            Console.WriteLine("Net amount is {0}", netAmount);
+        }
+
+        static double CalculateTaxes(double grossAmount)
+        {
+            if (grossAmount <= 1000)
+            {
+                return grossAmount;
+            }
+            else
+            {
+                double excessAmount = grossAmount - 1000;
+                double incomeTaxAmount = ((double)excessAmount / 100) * 10;
+                if(grossAmount > 3000)
+                {
+                    excessAmount = 2000; 
+                }
+                double socialTaxAmount = ((double)excessAmount / 100) * 15;
+
+                return grossAmount - incomeTaxAmount - socialTaxAmount;
+            }
+        }
+    }
+
+    // One way that additional taxes can be added programmaticaly is to capture the input from the console, asking the user to add any amount of additonal taxes and the amount that they should be taxed for
+    // and store them in a dictionary. Afterwards check if the gross amount is above 1000 and loop through each tax and apply it to the excess amount.
+}
